504b52c0cd2d16073ad22e50fad9c4aab7239cfd
Erich Schulz <62512797+ArtemStrelnikov@users.noreply.github.com>
2023-03-21 14:41:53 +0100

    Create Bus.java

---
+++ Bus.java
@@ -0,0 +1,36 @@
+package L7;
+
+public class Bus {
+	
+	public String model;
+	public String color;
+	public String Nummer;
+	
+	
+	public void showM() {
+		
+		System.out.println(model);
+
+}
+	
+	
+
+	
+	public void showColor() {
+		System.out.println(color);
+
+}
+
+
+	
+	public void showNummer() {
+		System.out.println(Nummer);
+
+}
+	
+	
+	
+	
+	
+	
+}
